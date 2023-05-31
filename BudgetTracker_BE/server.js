1bead3463ebcf87950819d02a1e5bc08ca0e4aaa
brandonwilbert8 <111483690+brandonwilbert8@users.noreply.github.com>
2023-05-31 11:24:53 +1000

    Added .env and connection function to DB on backend

--- BudgetTracker_BE/server.js
+++ BudgetTracker_BE/server.js
@@ -1,5 +1,22 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
+const uri = process.env.DB_CONNECTION;
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+connect();
 
 app.get('/api', (req, res) => {
   res.json('Welcome to Budget Tracker!');
