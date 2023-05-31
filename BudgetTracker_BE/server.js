9dc2d71695321f12f7264d5de01f7b1bee20cbcd
brandonwilbert8 <111483690+brandonwilbert8@users.noreply.github.com>
2023-05-31 11:45:14 +1000

    Added models & routes directories

--- BudgetTracker_BE/server.js
+++ BudgetTracker_BE/server.js
@@ -6,22 +6,23 @@ dotenv.config();
 
 const app = express();
 const uri = process.env.DB_CONNECTION;
+const port = process.env.PORT;
 
 async function connect() {
   try {
     await mongoose.connect(uri);
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB 📀');
   } catch (error) {
-    console.error(error);
+    console.error(`Error message: ${error}`);
   }
 }
 
 connect();
 
 app.get('/api', (req, res) => {
-  res.json('Welcome to Budget Tracker!');
+  res.json('Welcome to Budget Tracker! 💸');
 });
 
-app.listen(5000, () => {
-  console.log('Server started on port 5000');
+app.listen(port, () => {
+  console.log(`Server running on port: ${port} 🎉`);
 });
