e51d232ff18d8e74ebcabe8eff14e542e5f1df9a
brandonwilbert8 <111483690+brandonwilbert8@users.noreply.github.com>
2023-06-20 11:37:06 +1000

    Added eslint workingDirectories to eliminate errors

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -9,4 +9,8 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  'eslint.workingDirectories': [
+    {directory: 'client/', changeProcessCWD: true},
+    {directory: 'server/', changeProcessCWD: true},
+  ],
 };
